53e2c6544cebe623932c8a1d878fa873d2cc8dbd
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-02 15:23:55 +0530

    add execution part

--- task_6.sql
+++ task_6.sql
@@ -13,7 +13,7 @@ select * from task
 
 --INSERT 
 
---system procedures for insertion 
+drop procedure sp_insert_task
 
 CREATE PROCEDURE sp_insert_task
     @task_name VARCHAR(150),
@@ -25,14 +25,15 @@ CREATE PROCEDURE sp_insert_task
     @project_id INT
 AS
 BEGIN
-    SET NOCOUNT ON;
-
     INSERT INTO task (task_name, descriptions, starts_date, due_date, prioritys, statuss, project_id)
     VALUES (@task_name, @descriptions, @starts_date, @due_date, @prioritys, @statuss, @project_id);
+
+     SET NOCOUNT ON;
+       
 END
 GO
 
-exec sp_insert_task 'New Task', 'Description here', '2025-08-01', '2025-08-15', 'High', 'Pending', 1
+exec sp_insert_task 'New Task', 'Description here', '2025-08-01', '2025-08-15', 'High', 'Pending', 4
 
 
 select * from task
