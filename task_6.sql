f43cd8d5eabec6ccc6e5d07ee748386073436e4a
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-02 15:55:06 +0530

    added read UDF

--- task_6.sql
+++ task_6.sql
@@ -14,10 +14,11 @@ select * from task
 --INSERT 
 
 drop procedure sp_insert_task
+--in insert we use system procedure to create/insert the values to the system procedures
 
 CREATE PROCEDURE sp_insert_task
     @task_name VARCHAR(150),
-    @descriptions VARCHAR(255),
+    @descriptions VARCHAR(245),
     @starts_date DATE,
     @due_date DATE,
     @prioritys VARCHAR(150),
@@ -27,9 +28,6 @@ AS
 BEGIN
     INSERT INTO task (task_name, descriptions, starts_date, due_date, prioritys, statuss, project_id)
     VALUES (@task_name, @descriptions, @starts_date, @due_date, @prioritys, @statuss, @project_id);
-
-     SET NOCOUNT ON;
-       
 END
 GO
 
@@ -37,3 +35,25 @@ exec sp_insert_task 'New Task', 'Description here', '2025-08-01', '2025-08-15', 
 
 
 select * from task
+select * from project
+
+--DELETE FROM task 
+--WHERE task_name = 'New Task'
+
+
+--Read function
+-- we use use defined function for to show the data it can only readable only 
+
+CREATE FUNCTION project_details(@project_id INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT * 
+    FROM task
+    WHERE project_id = @project_id
+);
+
+SELECT * FROM project_details(4)
+
+
